aca98a9bc1bf3fa21df4316ea121772e02063208
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-07-09 18:43:04 +0200

    c first commit

---
+++ EZ-ECS/include/EZ_ECS.h
@@ -0,0 +1,77 @@
+#ifndef EZ_ECS_H
+#define EZ_ECS_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#ifndef EZ_ECS_MAX_ENTITIES
+#define EZ_ECS_MAX_ENTITIES 64
+#endif
+
+#ifndef EZ_ECS_MAX_COMPONENTS
+#define EZ_ECS_MAX_COMPONENTS 32
+#endif
+
+typedef struct ezecsScene {
+	void* sceneMatrix[EZ_ECS_MAX_ENTITIES][EZ_ECS_MAX_COMPONENTS];
+} ezecsScene;
+
+ezecsScene ezecsCreateScene() {
+	ezecsScene scene = { { NULL } };
+	return scene;
+}
+
+#define EZ_ECS_MAKE_COMPONENT_DEFINITIONS(x, n) \
+static const uint32_t ezecs ## x ## ComponentID = n; \
+x ## * ezecsAdd ## x ## Component(ezecsScene *scene, const uint32_t entity) { \
+	ezecsCheckEntitiesSize(entity);\
+	ezecsCheckComponentsSize(ezecs ## x ## ComponentID);\
+	x *component = (x*)calloc(1, sizeof(x));\
+	scene->sceneMatrix[entity][ezecs ## x ## ComponentID] = (void*)component;\
+	return (x ## *)component;\
+} \
+int ezecsHas ## x ## Component(const ezecsScene scene, const uint32_t entity) { \
+	return !(scene.sceneMatrix[entity][ezecs ## x ## ComponentID] == NULL); \
+} \
+x ## * ezecsGet ## x ## Component(const ezecsScene scene, const uint32_t entity) { \
+	return (x ## *)scene.sceneMatrix[entity][ezecs ## x ## ComponentID]; \
+} \
+void ezecsRemove ## x ## Component(ezecsScene *scene, const uint32_t entity) { \
+	free(scene->sceneMatrix[entity][ezecs ## x ## ComponentID]);\
+	scene->sceneMatrix[entity][ezecs ## x ## ComponentID] = NULL;\
+} \
+
+const uint32_t ezecsCreateEntity();
+
+void ezecsDestroyEntity(ezecsScene *scene, const uint32_t entity);
+
+void ezecsCheckEntitiesSize(const uint32_t entity);
+
+void ezecsCheckComponentsSize(const uint32_t componentID);
+
+
+const uint32_t ezecsCreateEntity() {
+	static uint32_t entity = 0;
+	const uint32_t _entity = entity;
+	entity++;
+	ezecsCheckEntitiesSize(_entity);
+	return _entity;
+}
+
+void ezecsDestroyEntity(ezecsScene *scene, const uint32_t entity) {
+	for (uint32_t i = 0; i < EZ_ECS_MAX_COMPONENTS; i++) {
+		free(scene->sceneMatrix[entity][i]);
+		scene->sceneMatrix[entity][i] = 0;
+	}
+}
+
+void ezecsCheckEntitiesSize(const uint32_t entity) {
+	assert(entity < EZ_ECS_MAX_ENTITIES);
+}
+
+void ezecsCheckComponentsSize(const uint32_t componentID) {
+	assert(componentID < EZ_ECS_MAX_COMPONENTS);
+}
+
+#endif
---
+++ Example/src/Example.c
@@ -0,0 +1,67 @@
+//max entities and max components can have any value, for the example i'll make them equal to 2
+#define EZ_ECS_MAX_ENTITIES 4
+#define EZ_ECS_MAX_COMPONENTS 4 //is the maximum number of component *types* used in total, in all ECSs, not for each object
+
+#include <EZ_ECS.h>
+#include<stdio.h>
+
+
+typedef struct Camera {
+
+	float FOV;
+	float aspectRatio;
+	float n;
+	float f;
+
+} Camera;
+EZ_ECS_MAKE_COMPONENT_DEFINITIONS(Camera, 0)
+
+typedef struct Transform {
+	
+	float position[3];
+	float rotation[3];
+	float scale[3];
+
+} Transform;
+EZ_ECS_MAKE_COMPONENT_DEFINITIONS(Transform, 1);
+
+typedef struct Player {
+
+	int strength;
+	int intelligence;
+	int luck;
+	int vitality;
+
+} Player;
+EZ_ECS_MAKE_COMPONENT_DEFINITIONS(Player, 2); 
+
+int main() {
+	
+
+	const uint32_t entity0 = ezecsCreateEntity();
+	const uint32_t entity1 = ezecsCreateEntity();
+	const uint32_t entity2 = ezecsCreateEntity();
+	const uint32_t entity3 = ezecsCreateEntity();
+
+	printf("Entities: %i, %i, %i, %i\n", entity0, entity1, entity2, entity3);
+
+	ezecsScene myScene = ezecsCreateScene();
+	
+	printf("Camera component ID: %i\n", ezecsCameraComponentID);
+	printf("Transform component ID: %i\n", ezecsTransformComponentID);
+	printf("Player component ID: %i\n", ezecsPlayerComponentID);
+
+	printf("entity0 has transform component: %i\n", ezecsHasTransformComponent(myScene, entity0));
+	Transform *transform = ezecsAddTransformComponent(&myScene, entity0);
+	transform->position[0] = 33.33f;
+	printf("entity0 has transform component: %i\n", ezecsHasTransformComponent(myScene, entity0));
+	
+	printf("entity0 transform x position: %f \n", ezecsGetTransformComponent(myScene, entity0)->position[0]);
+
+	ezecsRemoveTransformComponent(&myScene, entity0);
+	printf("entity0 has transform component: %i\n", ezecsHasTransformComponent(myScene, entity0));
+
+	ezecsDestroyEntity(&myScene, entity0);
+
+	return 0;
+}
