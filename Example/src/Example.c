da182f213e8b76372c099edea4e161aefcbb5aa7
Anas Boustani <sino3791@gmail.com>
2021-10-16 19:17:27 +0200

    scene structure

--- Example/src/Example.c
+++ Example/src/Example.c
@@ -44,13 +44,13 @@ FGG_ECS_MAKE_COMPONENT_DEFINITIONS(Material, 3)
 
 int main() {
 	
-	FggScene scene;
-	fggCreateScene(scene);
+	FggScene scene = { 0 };
+	fggCreateScene(&scene);
 
-	const uint32_t entity0 = fggCreateEntity();
-	const uint32_t entity1 = fggCreateEntity();
-	const uint32_t entity2 = fggCreateEntity();
-	const uint32_t entity3 = fggCreateEntity();
+	const uint32_t entity0 = fggCreateEntity(&scene);
+	const uint32_t entity1 = fggCreateEntity(&scene);
+	const uint32_t entity2 = fggCreateEntity(&scene);
+	const uint32_t entity3 = fggCreateEntity(&scene);
 	
 	printf("Entities: %i, %i, %i, %i\n", entity0, entity1, entity2, entity3);
 	printf("Camera component ID: %i\n", fggCameraID);
@@ -58,15 +58,15 @@ int main() {
 	printf("Player component ID: %i\n", fggPlayerID);
 
 	//entity0 stuff
-	fggAddTransform(scene, entity0)->position[0] = 33.33f;
-	fggAddCamera(scene, entity0)->FOV = 45.0f;
+	fggAddTransform(&scene, entity0)->position[0] = 33.33f;
+	fggAddCamera(&scene, entity0)->FOV = 45.0f;
 	
 	//entity1 stuff
-	fggAddPlayer(scene, entity1)->intelligence = -20;
+	fggAddPlayer(&scene, entity1)->intelligence = -20;
 
 	Material mat = { 1, "myShader.spv" };
-	fggSetMaterial(scene, &mat, entity0);
-	fggSetMaterial(scene, &mat, entity1);
+	fggSetMaterial(&scene, &mat, entity0);
+	fggSetMaterial(&scene, &mat, entity1);
 
 	mat.color = 2;
 	mat.shaderPath = "lol";
@@ -75,17 +75,17 @@ int main() {
 	printf("Transform id %i \n", fggTransformID);
 
 	for (uint32_t entity = 0; entity < FGG_ECS_MAX_ENTITIES; entity++) {
-		if (fggHasTransform(scene, entity) && !fggIsTransformShared(scene, entity)) {
-			printf("%f\n", fggGetTransform(scene, entity)->position[0]);
+		if (fggHasTransform(&scene, entity) && !fggIsTransformShared(&scene, entity)) {
+			printf("%f\n", fggGetTransform(&scene, entity)->position[0]);
 		}
-		if (fggHasCamera(scene, entity) && !fggIsCameraShared(scene, entity)) {
-			printf("%f\n", fggGetCamera(scene, entity)->FOV);
+		if (fggHasCamera(&scene, entity) && !fggIsCameraShared(&scene, entity)) {
+			printf("%f\n", fggGetCamera(&scene, entity)->FOV);
 		}
-		if (fggHasPlayer(scene, entity) && !fggIsPlayerShared(scene, entity)) {
-			printf("%i\n", fggGetPlayer(scene, entity)->intelligence);
+		if (fggHasPlayer(&scene, entity) && !fggIsPlayerShared(&scene, entity)) {
+			printf("%i\n", fggGetPlayer(&scene, entity)->intelligence);
 		}
-		if (fggHasMaterial(scene, entity) && fggIsMaterialShared(scene, entity)) {
-			printf("%i, %s\n", fggGetMaterial(scene, entity)->color, fggGetMaterial(scene, entity)->shaderPath);
+		if (fggHasMaterial(&scene, entity) && fggIsMaterialShared(&scene, entity)) {
+			printf("%i, %s\n", fggGetMaterial(&scene, entity)->color, fggGetMaterial(&scene, entity)->shaderPath);
 		}
 	}
 
--- FggECS/include/fggECS.h
+++ FggECS/include/fggECS.h
@@ -19,77 +19,81 @@
 #define FGG_ECS_SHARED_COMPONENTS FGG_ECS_MAX_COMPONENTS
 #endif
 
-typedef void* FggScene[FGG_ECS_MAX_ENTITIES][FGG_ECS_MAX_COMPONENTS + FGG_ECS_SHARED_COMPONENTS];
+//typedef void* FggScene[FGG_ECS_MAX_ENTITIES][FGG_ECS_MAX_COMPONENTS + FGG_ECS_SHARED_COMPONENTS];
 
+typedef struct FggScene {
+
+	uint32_t entity_count;
+	void* matrix[FGG_ECS_MAX_ENTITIES][FGG_ECS_MAX_COMPONENTS + FGG_ECS_SHARED_COMPONENTS];
+
+} FggScene;
 
 #define FGG_ECS_MAKE_COMPONENT_DEFINITIONS(x, n) \
 static const uint32_t fgg ## x ## ID = n; \
 static const uint32_t fgg ## x ## Size = sizeof(x); \
-static x* fggAdd ## x(FggScene scene, const uint32_t entity) { \
+static x* fggAdd ## x(FggScene* scene, const uint32_t entity) { \
 	fggCheckEntitiesSize(entity);\
 	fggCheckComponentsSize(fgg ## x ## ID);\
 	x* component = (x*)calloc(1, sizeof(x));\
-	scene[entity][fgg ## x ## ID] = component;\
+	scene->matrix[entity][fgg ## x ## ID] = component;\
 	return component;\
 } \
-static x* fggSet ## x(FggScene scene, x* component, const uint32_t entity) {\
+static x* fggSet ## x(FggScene* scene, x* component, const uint32_t entity) {\
 	fggCheckEntitiesSize(entity);\
 	fggCheckComponentsSize(fgg ## x ## ID);\
-	scene[entity][fgg ## x ## ID] = component;\
+	scene->matrix[entity][fgg ## x ## ID] = component;\
 	uint32_t* shared = calloc(1, sizeof(int));\
 	if (shared != NULL) {\
 		*shared = fgg ## x ## ID;\
 	}\
-	scene[entity][fgg ## x ## ID + FGG_ECS_SHARED_COMPONENTS] = shared;\
+	scene->matrix[entity][fgg ## x ## ID + FGG_ECS_SHARED_COMPONENTS] = shared;\
 	return component;\
 }\
-static int fggIs ## x ## Shared(FggScene scene, const uint32_t entity) {\
-		return !(scene[entity][fgg ## x ## ID + FGG_ECS_SHARED_COMPONENTS] == NULL); \
+static int fggIs ## x ## Shared(FggScene* scene, const uint32_t entity) {\
+		return !(scene->matrix[entity][fgg ## x ## ID + FGG_ECS_SHARED_COMPONENTS] == NULL); \
 }\
-static int fggHas ## x(const FggScene scene, const uint32_t entity) { \
-	return !(scene[entity][fgg ## x ## ID] == NULL); \
+static int fggHas ## x(const FggScene* scene, const uint32_t entity) { \
+	return !(scene->matrix[entity][fgg ## x ## ID] == NULL); \
 } \
-static x* fggGet ## x(const FggScene scene, const uint32_t entity) { \
-	return (x*)scene[entity][fgg ## x ## ID]; \
+static x* fggGet ## x(const FggScene* scene, const uint32_t entity) { \
+	return (x*)scene->matrix[entity][fgg ## x ## ID]; \
 } \
-static void FggECSRemove ## x(FggScene scene, const uint32_t entity) { \
-	free(scene[entity][fgg ## x ## ID]);\
-	scene[entity][fgg ## x ## ID] = NULL;\
+static void FggECSRemove ## x(FggScene* scene, const uint32_t entity) { \
+	free(scene->matrix[entity][fgg ## x ## ID]);\
+	scene->matrix[entity][fgg ## x ## ID] = NULL;\
 } \
 
 
-static void fggCreateScene(FggScene scene);
+static void fggCreateScene(FggScene* scene);
 
-static const uint32_t fggCreateEntity();
+static const uint32_t fggCreateEntity(FggScene* scene);
 
-static void fggDestroyEntity(FggScene scene, const uint32_t entity);
+static void fggDestroyEntity(FggScene* scene, const uint32_t entity);
 
 static void fggCheckEntitiesSize(const uint32_t entity);
 
 static void fggCheckComponentsSize(const uint32_t componentID);
 
-static void fggClearScene(FggScene scene);
+static void fggClearScene(FggScene* scene);
 
-void fggCreateScene(FggScene scene) {
+void fggCreateScene(FggScene* scene) {
 	for (uint32_t entity = 0; entity < FGG_ECS_MAX_ENTITIES; entity++) {
 		for (uint32_t component = 0; component < FGG_ECS_MAX_COMPONENTS + FGG_ECS_SHARED_COMPONENTS; component++) {
-			scene[entity][component] = NULL;
+			scene->matrix[entity][component] = NULL;
 		}
 	}
 }
 
-const uint32_t fggCreateEntity() {
-	static uint32_t entity = 0;
-	const uint32_t _entity = entity;
-	entity++;
-	fggCheckEntitiesSize(_entity);
-	return _entity;
+const uint32_t fggCreateEntity(FggScene* scene) {
+	fggCheckEntitiesSize(scene->entity_count);
+	scene->entity_count++;
+	return scene->entity_count - 1;
 }
 
-void fggDestroyEntity(FggScene scene, const uint32_t entity) {
+void fggDestroyEntity(FggScene* scene, const uint32_t entity) {
 	for (uint32_t component = 0; component < FGG_ECS_MAX_COMPONENTS + FGG_ECS_SHARED_COMPONENTS; component++) {
-		free(scene[entity * FGG_ECS_MAX_ENTITIES + component]);
-		scene[entity][component] = NULL;
+		free(scene->matrix[entity * FGG_ECS_MAX_ENTITIES + component]);
+		scene->matrix[entity][component] = NULL;
 	}
 }
 
@@ -101,8 +105,8 @@ void fggCheckComponentsSize(const uint32_t componentID) {
 	assert(componentID < FGG_ECS_MAX_COMPONENTS);
 }
 
-void fggClearScene(FggScene scene) {
-	free(scene);
+void fggClearScene(FggScene* scene) {
+	free(scene->matrix);
 }
 
 #endif
